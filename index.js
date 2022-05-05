b76c7aeb0f6c469d30f55104a21fafbade22b68b
MattoDev <mateofonseca.profesional@gmail.com>
2022-05-04 20:40:09 -0500

    Create search class

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 const main = async () => {
   let opt;
+  const busquedas = new Busquedas();
 
   do {
     opt = await inquirerMenu();
-    console.log({ opt });
+
+    switch (opt) {
+      case 1:
+        // Mostrar mensaje
+        const lugar = await leerInput("Ciudad: ");
+        console.log(lugar);
+
+        //Buscar  lugares
+
+        //Seleccionar el lugar
+
+        //Clima
+
+        //Mostrar resultados
+        console.log("\nInformacion de la ciudad\n".green);
+        console.log("Ciudad:");
+        console.log("Ciudad;");
+        console.log("Lat:");
+        console.log("Lng:");
+        console.log("Temperatura:");
+        console.log("Minima:");
+        console.log("Maximas:");
+        break;
+
+      default:
+        break;
+    }
 
     if (opt !== 0) await pausa();
   } while (opt !== 0);
---
+++ models/busquedas.js
@@ -0,0 +1,15 @@
+class Busquedas {
+  historial = ["tegucigalpa", "Madrid", "San Jose"];
+
+  constructor() {
+    //TODO: leer DB si existe
+  }
+  async ciudad(lugar = "") {
+    // peticion http
+    console.log(lugar);
+
+    return []; //retornar los lugares
+  }
+}
+
+module.exports = Busquedas;
