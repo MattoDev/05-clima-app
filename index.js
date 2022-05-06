60702fb6ade8f5f1a3c2f8ca0256b86d5c745374
MattoDev <mateofonseca.profesional@gmail.com>
2022-05-05 20:30:05 -0500

    acces to city name ltd and lng

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const {
+  leerInput,
+  inquirerMenu,
+  pausa,
+  listarLugares,
+} = require("./helpers/inquirer");
 const Busquedas = require("./models/busquedas");
 
 // console.log(process.env.MAPBOX_KEY);
@@ -15,21 +20,23 @@ const main = async () => {
     switch (opt) {
       case 1:
         // Mostrar mensaje
-        const lugar = await leerInput("Ciudad: ");
-        await busquedas.ciudad(lugar);
+        const termino = await leerInput("Ciudad: ");
+        //Buscar los lugares
+        const lugares = await busquedas.ciudad(termino);
+        // Seleccionar el lugar
+        const id = await listarLugares(lugares);
+        const lugarSel = lugares.find((l) => l.id === id);
+        // console.log(lugarSel);
 
-        //Buscar  lugares
-
-        //Seleccionar el lugar
+        // console.log({ id });
 
         //Clima
 
         //Mostrar resultados
         console.log("\nInformacion de la ciudad\n".green);
-        console.log("Ciudad:");
-        console.log("Ciudad;");
-        console.log("Lat:");
-        console.log("Lng:");
+        console.log("Ciudad: ", lugarSel.nombre);
+        console.log("Lat: ", lugarSel.lat);
+        console.log("Lng: ", lugarSel.lng);
         console.log("Temperatura:");
         console.log("Minima:");
         console.log("Maximas:");
